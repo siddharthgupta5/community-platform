46d805779f1ce866f881c4b62134585e27f574f0
SIDDHARTHGUPTA <contactsid14@gmail.com>
2025-05-16 20:06:00 +0530

    User schema updated

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const snowflake = require('@theinternetfolks/snowflake');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
   id: {
@@ -9,21 +10,44 @@ const userSchema = new mongoose.Schema({
   },
   name: {
     type: String,
-    maxlength: 64,
-    default: null,
+    required: [true, 'Please provide a name'],
+    trim: true,
+    maxlength: [64, 'Name cannot be more than 64 characters'],
+    validate: {
+      validator: function(v) {
+        return /^[a-zA-Z\s]*$/.test(v);
+      },
+      message: 'Name can only contain letters and spaces'
+    }
   },
   email: {
     type: String,
     required: true,
     unique: true,
-    maxlength: 128,
+    lowercase: true,
+    maxlength: [128, 'Email cannot be more than 128 characters'],
+    validate: [validator.isEmail, 'Please provide a valid email']
   },
   password: {
     type: String,
-    required: true,
-    maxlength: 64,
+    required: [true, 'Please provide a password'],
+    minlength: [6, 'Password must be at least 6 characters'],
     select: false,
+    validate: {
+      validator: function(v) {
+        return /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/.test(v);
+      },
+      message: 'Password must contain at least one letter and one number'
+    }
   },
+  isEmailVerified: {
+    type: Boolean,
+    default: false
+  },
+  emailVerificationToken: String,
+  emailVerificationExpires: Date,
+  resetPasswordToken: String,
+  resetPasswordExpires: Date,
   created_at: {
     type: Date,
     default: Date.now,
@@ -32,6 +56,16 @@ const userSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+});
+
+// Create indexes
+userSchema.index({ email: 1 });
+userSchema.index({ id: 1 });
+
+// Update timestamps on save
+userSchema.pre('save', function(next) {
+  this.updated_at = Date.now();
+  next();
 });
 
 module.exports = mongoose.model('User', userSchema);
