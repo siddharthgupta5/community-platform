d7e7ba7c5c808b59d4833a1c226e5bf10d18cb77
SIDDHARTHGUPTA <contactsid14@gmail.com>
2025-05-16 20:55:08 +0530

    member routes fixed

--- routes/memberRoutes.js
+++ routes/memberRoutes.js
@@ -4,6 +4,7 @@ const memberController = require('../controllers/memberController');
 const authController = require('../controllers/authController');
 const { validateMember } = require('../utils/validators');
 
+// Only Admin can add members
 router.post(
   '/',
   authController.protect,
@@ -12,12 +13,12 @@ router.post(
   memberController.addMember
 );
 
+// Only Admin can remove members
 router.delete(
   '/:id',
   authController.protect,
-  authController.isAdminOrModerator,
+  authController.isCommunityAdmin,
   memberController.removeMember
 );
 
 module.exports = router;
-
