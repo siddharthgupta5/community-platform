26dcebf83493a2b0356e93e5f1d240b2b012da1c
SIDDHARTHGUPTA <contactsid14@gmail.com>
2025-05-17 12:52:11 +0530

    authorization routes fixed

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 const { validateSignup, validateSignin } = require('../utils/validators');
+const { protect } = require('../middleware/auth');
 
 router.post('/signup', validateSignup, authController.signup);
 router.post('/signin', validateSignin, authController.signin);
-router.get('/me', authController.protect, authController.getMe);
+router.get('/me', protect, authController.getMe);
 
 module.exports = router;
