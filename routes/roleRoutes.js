d65f921db3b93162c71c35e42065882b1095a7d2
SIDDHARTHGUPTA <contactsid14@gmail.com>
2025-05-17 12:56:06 +0530

    role routes fixed

--- routes/roleRoutes.js
+++ routes/roleRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const roleController = require('../controllers/roleController');
-const authController = require('../controllers/authController');
+const { protect } = require('../middleware/auth');
 
-router.post('/', authController.protect, roleController.createRole);
+router.post('/', protect, roleController.createRole);
 router.get('/', roleController.getRoles);
 
 module.exports = router;
