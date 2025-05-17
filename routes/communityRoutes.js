6e597c484c09191ee419ee4d26f7997c6dcfcf34
SIDDHARTHGUPTA <contactsid14@gmail.com>
2025-05-17 12:59:46 +0530

    community routes modified and fixed

--- routes/communityRoutes.js
+++ routes/communityRoutes.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const communityController = require('../controllers/communityController');
-const authController = require('../controllers/authController');
+const { protect } = require('../middleware/auth');
 const { validateCommunity } = require('../utils/validators');
 
 router.get('/', communityController.getCommunities);
-router.post('/', authController.protect, validateCommunity, communityController.createCommunity);
+router.post('/', protect, validateCommunity, communityController.createCommunity);
 router.get('/:id/members', communityController.getCommunityMembers);
-router.get('/me/owner', authController.protect, communityController.getMyOwnedCommunities);
-router.get('/me/member', authController.protect, communityController.getMyJoinedCommunities);
+router.get('/me/owner', protect, communityController.getMyOwnedCommunities);
+router.get('/me/member', protect, communityController.getMyJoinedCommunities);
 
 module.exports = router;
